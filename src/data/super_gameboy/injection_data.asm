4bb393bb9c20ecfa0e6dccdc8e2788dc3b1782b5
Christopher Hartman <cphartman@gmail.com>
2023-12-11 22:55:20 -0500

    Fixes relative labels

--- src/data/super_gameboy/injection_data.asm
+++ src/data/super_gameboy/injection_data.asm
@@ -1,9 +1,12 @@
 Awakening_Patch_Data:
 Awakening_Patch_Data_0:
 	sgb_data_send_cmd $0000, $7F, 11
-	db $a9, $04, $8f, $00, $0f, $7f, $5c, $14, $08, $00, $de
+	db $a9, $04, $8f, $00, $0f, $7f, $4c, $11, $00, $de, $ad
 Awakening_Patch_Data_1:
 	sgb_data_send_cmd $000b, $7F, 11
-	db $ad, $be, $ef, $13, $37, $13, $37, $00, $00, $00, $00
+	db $be, $ef, $13, $37, $13, $37, $5c, $14, $08, $00, $00
+Awakening_Patch_Data_2:
+	sgb_data_send_cmd $0016, $7F, 11
+	db $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00
 
-AWAKENING_LOAD_PACKETS = 2
+AWAKENING_LOAD_PACKETS = 3
--- src/data/super_gameboy/injection_data/injection_script.asm
+++ src/data/super_gameboy/injection_data/injection_script.asm
@@ -1,21 +1,25 @@
-; Minimal example of using ca65 to build SNES ROM.
-
 .p816   ; 65816 processor
 .i16    ; X/Y are 16 bits
 .a8     ; A is 8 bits
 
 .include "snes.inc"
-
 .segment "CODE"
 CODE:
 
-   ; Check if we've done init
+   ; testing memory writing
    lda #$04
    sta $7f0F00
    
+   ; Testing injected label addresses
+   JMP @injected_label
+
+@some_data:
+   .byte $DE,$AD,$BE,$EF
+   .byte $13,$37
+   .byte $13,$37
+
+@injected_label:
    JML $000814
 
-   .byte $DE,$AD,$BE,$EF
-   .byte $13,$37,$13,$37
+   ; EOF marker
    .byte 0,0,0
-
