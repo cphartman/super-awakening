094109508e58c9f1e338acef2c6f64aa9f0bd4f8
Christopher Hartman <cphartman@gmail.com>
2023-12-16 18:06:38 -0500

    Un-map SNES keys that are translated to GB Keys

--- src/data/super_gameboy/injection_data.asm
+++ src/data/super_gameboy/injection_data.asm
@@ -1,42 +1,51 @@
 Awakening_Patch_Data:
 Awakening_Patch_Data_0:
 	sgb_data_send_cmd $0000, $7F, 11
-	db $e2, $20, $ad, $11, $0f, $c9, $10, $d0, $08, $ad, $14
+	db $e2, $20, $ad, $11, $0f, $29, $10, $c9, $10, $d0, $10
 Awakening_Patch_Data_1:
 	sgb_data_send_cmd $000b, $7F, 11
-	db $0f, $09, $10, $8d, $14, $0f, $ad, $11, $0f, $c9, $20
+	db $ad, $14, $0f, $09, $10, $8d, $14, $0f, $ad, $11, $0f
 Awakening_Patch_Data_2:
 	sgb_data_send_cmd $0016, $7F, 11
-	db $d0, $08, $ad, $14, $0f, $09, $20, $8d, $14, $0f, $ad
+	db $29, $ef, $8d, $11, $0f, $ad, $11, $0f, $29, $20, $c9
 Awakening_Patch_Data_3:
 	sgb_data_send_cmd $0021, $7F, 11
-	db $11, $0f, $c9, $40, $d0, $10, $ad, $14, $0f, $09, $08
+	db $20, $d0, $10, $ad, $14, $0f, $09, $20, $8d, $14, $0f
 Awakening_Patch_Data_4:
 	sgb_data_send_cmd $002c, $7F, 11
-	db $8d, $14, $0f, $ad, $11, $0f, $29, $bf, $8d, $11, $0f
+	db $ad, $11, $0f, $29, $df, $8d, $11, $0f, $ad, $11, $0f
 Awakening_Patch_Data_5:
 	sgb_data_send_cmd $0037, $7F, 11
-	db $ad, $12, $0f, $c9, $40, $d0, $10, $ad, $14, $0f, $09
+	db $29, $40, $c9, $40, $d0, $10, $ad, $14, $0f, $09, $08
 Awakening_Patch_Data_6:
 	sgb_data_send_cmd $0042, $7F, 11
-	db $02, $8d, $14, $0f, $ad, $12, $0f, $29, $bf, $8d, $12
+	db $8d, $14, $0f, $ad, $11, $0f, $29, $bf, $8d, $11, $0f
 Awakening_Patch_Data_7:
 	sgb_data_send_cmd $004d, $7F, 11
-	db $0f, $ad, $11, $0f, $c9, $80, $d0, $10, $ad, $14, $0f
+	db $ad, $12, $0f, $29, $40, $c9, $40, $d0, $10, $ad, $14
 Awakening_Patch_Data_8:
 	sgb_data_send_cmd $0058, $7F, 11
-	db $09, $01, $8d, $14, $0f, $ad, $11, $0f, $29, $7f, $8d
+	db $0f, $09, $02, $8d, $14, $0f, $ad, $12, $0f, $29, $bf
 Awakening_Patch_Data_9:
 	sgb_data_send_cmd $0063, $7F, 11
-	db $11, $0f, $ad, $12, $0f, $c9, $80, $d0, $10, $ad, $14
+	db $8d, $12, $0f, $ad, $11, $0f, $29, $80, $c9, $80, $d0
 Awakening_Patch_Data_10:
 	sgb_data_send_cmd $006e, $7F, 11
-	db $0f, $09, $04, $8d, $14, $0f, $ad, $12, $0f, $29, $7f
+	db $10, $ad, $14, $0f, $09, $01, $8d, $14, $0f, $ad, $11
 Awakening_Patch_Data_11:
 	sgb_data_send_cmd $0079, $7F, 11
-	db $8d, $12, $0f, $5c, $0c, $08, $00, $08, $e2, $30, $28
+	db $0f, $29, $7f, $8d, $11, $0f, $ad, $12, $0f, $29, $80
 Awakening_Patch_Data_12:
 	sgb_data_send_cmd $0084, $7F, 11
-	db $5c, $7c, $00, $7f, $00, $00, $00, $00, $00, $00, $00
+	db $c9, $80, $d0, $10, $ad, $14, $0f, $09, $04, $8d, $14
+Awakening_Patch_Data_13:
+	sgb_data_send_cmd $008f, $7F, 11
+	db $0f, $ad, $12, $0f, $29, $7f, $8d, $12, $0f, $5c, $0c
+Awakening_Patch_Data_14:
+	sgb_data_send_cmd $009a, $7F, 11
+	db $08, $00, $08, $e2, $30, $28, $5c, $98, $00, $7f, $00
+Awakening_Patch_Data_15:
+	sgb_data_send_cmd $00a5, $7F, 11
+	db $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00
 
-AWAKENING_LOAD_PACKETS = 13
+AWAKENING_LOAD_PACKETS = 16
