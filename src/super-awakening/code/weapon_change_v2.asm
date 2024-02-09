402f3e14ef80965fd84fe71a03f0bc885a814b90
Christopher Hartman <cphartman@gmail.com>
2024-02-09 16:33:44 -0500

    Ocarina weapon change. Skip other weapon on change

--- src/super-awakening/code/weapon_change_v2.asm
+++ src/super-awakening/code/weapon_change_v2.asm
@@ -9,7 +9,7 @@ SuperAwakening_ChangeWeapon_v2::
     jp nz, return
 
 ; Parameters
-; change_weapon( Weapon_Number, Button )
+; change_weapon( Weapon_Number, Button, Other_Weapon_Number )
 MACRO change_weapon
 change_weapon_\1: 
 
@@ -21,7 +21,88 @@ change_weapon_\1:
         cp \2
         jp nz, .change_weapon_\1_end
 
+        ; Check if the orcarina index should be incremented
+    .ocarina_\1
+        ; Check if we have ocarina equiped
+        ld hl, wSuperAwakening.Weapon\1_Value
+        ld a, [hl]
+        cp INVENTORY_OCARINA
+        jp nz, .ocarina_\1_end
 
+        ; Check if we're already on the last song
+        ld a, [wSelectedSongIndex]
+        cp SONG_SELECTED_INDEX_MAX
+        jp z, .ocarina_\1_reset
+
+        ; Increment the song index
+        inc a
+        ld [wSelectedSongIndex], a
+
+    .ocarina_\1_test_song_2
+        ; Check if we're trying to switch to song 2
+        cp MANBO_MAMBO_SELECTED_INDEX
+        jp nz, .ocarina_\1_test_song_3
+        ; Check if we have song 2 unlocked
+        ld a, [wOcarinaSongFlags]
+        and MANBO_MAMBO_FLAG
+        cp MANBO_MAMBO_FLAG
+        jp nz, .ocarina_\1_test_song_2_locked
+        ; Song is unlocked, we can exit
+        jp return
+
+    .ocarina_\1_test_song_2_locked
+        ; Song 2 is locked, so see if we can use song 3
+        ld a, FROGS_SONG_OF_THE_SOUL_SELECTED_INDEX
+        ld [wSelectedSongIndex], a
+        
+    .ocarina_\1_test_song_3
+        ; Check if we have song 3 unlocked
+        ld a, [wOcarinaSongFlags]
+        and FROGS_SONG_OF_THE_SOUL_FLAG
+        cp FROGS_SONG_OF_THE_SOUL_FLAG
+        jp nz, .ocarina_\1_reset ; Song 3 not unlocked, reset the song index
+        ; Song is unlocked, we can exit
+        jp return
+
+    .ocarina_\1_reset
+    
+    ; check if no songs unlocked
+    .ocarina_\1_reset_song_0
+        ld a, [wOcarinaSongFlags]
+        cp $00
+        jp nz, .ocarina_\1_reset_song_1
+        ld hl, wSelectedSongIndex
+        ld [hl], 0
+        jp .ocarina_\1_end
+    
+    ; Check if song 1 is unlocked
+    .ocarina_\1_reset_song_1
+        ld a, [wOcarinaSongFlags]
+        and BALLAD_OF_THE_WIND_FISH_FLAG
+        cp BALLAD_OF_THE_WIND_FISH_FLAG
+        jp nz, .ocarina_\1_reset_song_2
+        ld hl, wSelectedSongIndex
+        ld [hl], BALLAD_OF_THE_WIND_FISH_SELECTED_INDEX
+        jp .ocarina_\1_end
+
+    ; Check if song 1 is unlocked
+    .ocarina_\1_reset_song_2
+        ld a, [wOcarinaSongFlags]
+        and MANBO_MAMBO_FLAG
+        cp MANBO_MAMBO_FLAG
+        jp nz, .ocarina_\1_reset_song_3
+        ld hl, wSelectedSongIndex
+        ld [hl], MANBO_MAMBO_SELECTED_INDEX
+        jp .ocarina_\1_end
+
+    ; If you don't have none, 1 or 2, you must have song 3 (...?)
+    .ocarina_\1_reset_song_3
+        ld hl, wSelectedSongIndex
+        ld [hl], FROGS_SONG_OF_THE_SOUL_SELECTED_INDEX
+
+.ocarina_\1_end
+
+        ; Try to increment the weapon index
         ; while( d < SUPER_AWAKENING_INVENTORY_SLOT_COUNT+1 )
         ld d, 0 
     .next_weapon_\1_loop
@@ -42,13 +123,19 @@ change_weapon_\1:
         ld a, 0
 
     .next_weapon_\1_test
-        ; Store the index to RAM and [bc]
+        ; Store the index to RAM
         ld  [wSuperAwakening.Weapon\1_Inventory_Index], a
-        ld c, a
-        ld b, $00
+
+        ; Check if this is the same index as the other weapon index
+        ld hl, wSuperAwakening.Weapon\3_Inventory_Index
+        ld b, [hl]
+        cp b
+        jp z, .next_weapon_\1_loop
 
         ; Check if weapon is hidden in inventory
         ld hl, wSuperAwakening.Items_Hidden
+        ld c, a ; [a] is the weapon index
+        ld b, $00
         add hl, bc
         ld a, [hl]
         cp 1
@@ -87,8 +174,8 @@ change_weapon_\1:
 .change_weapon_\1_end
 ENDM
 
-    change_weapon 3,J_RIGHT
-    change_weapon 4,J_LEFT
+    change_weapon 3,J_RIGHT,4
+    change_weapon 4,J_LEFT,3
 
 
 return:
